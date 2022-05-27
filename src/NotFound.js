9e8c21ce6cf12554d1cd8aa97dc168764a991881
Nathan Conn <nconn34@gmail.com>
2022-05-27 12:50:51 -0700

    Second commit

---
+++ src/NotFound.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+    return ( 
+        <div className = "not-found">
+            <h2>Sorry!</h2>
+            <p>Page cannot be found</p>
+            <Link to="/">Head back to the homepage...</Link>
+        </div>
+     );
+}
+ 
+export default NotFound;
